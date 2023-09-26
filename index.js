1bcca73f0f46deac4d196ff095d15663c8ed6c68
Nour kayyali <nourkayyali91@gmail.com>
2023-09-26 22:13:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+
+const num1=2;
+const num2=31;
+const multiply=num1*num2;
+let random=Math.floor(Math.random() * 10) + 1;
+const num3=64;
+const num4=5;
+const mod=num3%num4;
+const max=Math.max(10 , 20 , 4);
